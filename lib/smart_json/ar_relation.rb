1820a079b5873703b866168c5df9f329703554d6
tompng <tomoyapenguin@gmail.com>
2015-02-03 17:14:58 +0900

    remove unnecesary option to method

--- lib/smart_json/ar_relation.rb
+++ lib/smart_json/ar_relation.rb
@@ -6,7 +6,7 @@ module SmartJSON::ARRelation
       records = records.includes(SmartJSON::Util.hash_to_includes_options includes)
     end
     records.map do |model|
-      model.as_styled_smart_json definitions, default: true
+      model.as_styled_smart_json definitions
     end
   end
   def as_smart_json *options
